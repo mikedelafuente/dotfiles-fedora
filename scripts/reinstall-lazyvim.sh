0acbf2aa8be736dcf09592ab4aa087863fe3084d
Mike de la Fuente <mike.delafuente@gmail.com>
2025-11-07 00:38:22 -0700

    Delete existing backups during reinstall of neovim

--- scripts/reinstall-lazyvim.sh
+++ scripts/reinstall-lazyvim.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # required
+# remove existing config backup if it exists
+rm -rf ~/.config/nvim.bak
 mv ~/.config/nvim{,.bak} --force
 
 # optional but recommended
+rm -rf ~/.local/share/nvim.bak
+rm -rf ~/.local/state/nvim.bak
+rm -rf ~/.cache/nvim.bak
+
 mv ~/.local/share/nvim{,.bak} --force
 mv ~/.local/state/nvim{,.bak} --force
 mv ~/.cache/nvim{,.bak} --force
